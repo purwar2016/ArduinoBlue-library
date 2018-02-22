4f40ea5be85f809989d8291ba3277a94c26a63fe
jaean123 <whileloops@gmail.com>
2018-02-21 20:37:10 -0500

    Update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -6,61 +6,62 @@
 #include <ArduinoBlue.h>
 
 
-// List of supported pins: https://www.arduino.cc/en/Reference/SoftwareSerial
+// The bluetooth tx and rx pins must be supported by software serial.
+// Visit https://www.arduino.cc/en/Reference/SoftwareSerial for unsupported pins.
 // Bluetooth TX -> Arduino D8
-const int BLUETOOTH_TX = 8;
+const int BLUETOOTH_TX = 11;
 // Bluetooth RX -> Arduino D7
-const int BLUETOOTH_RX = 7;
+const int BLUETOOTH_RX = 10;
 
-// variables
-int prevThrottle = 49, prevSteering = 49, // 49 is the still value for the throttle and steering
-        throttle, steering, prevMillis, sliderVal, button, sliderId;
+int prevThrottle = 49;
+int prevSteering = 49;
+int throttle, steering, sliderVal, button, sliderId;
 
 SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
 ArduinoBlue phone(bluetooth); // pass reference of bluetooth object to ArduinoCommander.
 
-// Setup code runs once after program starts
+// Setup code runs once after program starts.
 void setup() {
-    // Start serial monitor at 9600 bps
+    // Start serial monitor at 9600 bps.
     Serial.begin(9600);
 
-    // Start bluetooth serial at 9600 bps
+    // Start bluetooth serial at 9600 bps.
     bluetooth.begin(9600);
 
-    // delay just in case bluetooth module needs time to "get ready"
+    // delay just in case bluetooth module needs time to "get ready".
     delay(100);
 
     Serial.println("setup complete");
-    prevMillis = millis();
 }
 
 // Put your main code here, to run repeatedly:
 void loop() {
-    // ID of the button pressed pressed
+    // ID of the button pressed pressed.
     button = phone.getButton();
 
     // Returns the text data sent from the phone.
-    // After it returns the latest data, empty string "" is sent in subsequent
+    // After it returns the latest data, empty string "" is sent in subsequent.
     // calls until text data is sent again.
     String str = phone.getText();
 
-    // throttle and steering values go from 0 to 99.
+    // Throttle and steering values go from 0 to 99.
+    // When throttle and steering values are at 99/2 = 49, the joystick is at center.
     throttle = phone.getThrottle();
     steering = phone.getSteering();
 
-    // ID of the slider moved
+    // ID of the slider moved.
     sliderId = phone.getSliderId();
 
-    // slider value goes from 0 to 200
+    // Slider value goes from 0 to 200.
     sliderVal = phone.getSliderVal();
 
-    // display button data whenever its pressed
+    // Display button data whenever its pressed.
     if (button != -1) {
         Serial.print("Button: ");
         Serial.println(button);
     }
 
-    // display slider data when slider moves
+    // Display slider data when slider moves
     if (sliderId != -1) {
         Serial.print("Slider ID: ");
         Serial.print(sliderId);
@@ -68,22 +69,23 @@ void loop() {
         Serial.println(sliderVal);
     }
 
-    // display throttle and steering data if steering or throttle value is changed
+    // Display throttle and steering data if steering or throttle value is changed
     if (prevThrottle != throttle || prevSteering != steering) {
-        Serial.print("Throttle: "); Serial.print(throttle); Serial.print(", Steering: "); Serial.println(steering);
+        Serial.print("Throttle: "); Serial.print(throttle); Serial.print("\tSteering: "); Serial.println(steering);
         prevThrottle = throttle;
         prevSteering = steering;
     }
 
+    // If a text from the phone was sent print it to the serial monitor
     if (str != "") {
         Serial.println(str);
     }
 
     // Send string from serial command line to the phone. This will alert the user.
     if (Serial.available()) {
-        Serial.write("usb: ");
+        Serial.write("send: ");
         String str = Serial.readString();
-        phone.sendMessage(str);
+        phone.sendMessage(str); // phone.sendMessage(str) sends the text to the phone.
         Serial.print(str);
         Serial.write('\n');
     }
