e03687a2a37ecdab444704abf1f26f37c317f440
jaean123 <whileloops@gmail.com>
2018-06-18 21:39:30 -0400

    Update two-wheel-drive-robot.ino

--- examples/two-wheel-drive-robot/two-wheel-drive-robot.ino
+++ examples/two-wheel-drive-robot/two-wheel-drive-robot.ino
@@ -31,7 +31,7 @@ Servo leftServo;
 Servo rightServo;
 
 SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
-ArduinoBlue phone(bluetooth);
+ArduinoBlue phone(bluetooth); // pass reference of bluetooth object to ArduinoBlue constructor.
 
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
